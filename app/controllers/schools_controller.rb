fcc6f3473ec195df1ca5606f2a1e799f6470871e
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-04-29 12:44:39 -0600

    added update/edit/destroy methods in controller

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,7 +1,6 @@
 class SchoolsController < ApplicationController
   def index
     @schools = School.all
-
   end
 
   def show
@@ -9,12 +8,26 @@ class SchoolsController < ApplicationController
   end
 
   def new
-
   end
 
   def create
     school = School.create(params)
 
     redirect_to school_path(school.id)
+  end
+
+  def edit
+    @school = School.find(params[:id])
+  end
+
+  def update
+    School.update(params[:id], params)
+    redirect_to school_path(params[:id])
+  end
+
+  def destroy
+    School.destroy(params[:id])
+
+    redirect_to schools_path
   end
 end
