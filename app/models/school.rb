39326a4d5ba03da53883cdc108e0ecfc7b4389b9
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-04-29 10:16:12 -0600

    assigned the attributes to method calls for school object

--- app/models/school.rb
+++ app/models/school.rb
@@ -5,11 +5,20 @@ class School
   end
 
   def self.all
-    service.schools
+    new(service.schools)
   end
 
   def self.find(id)
-    service.school(id)
+    new(service.school(id))
+  end
+
+  attr_reader :uid, :id, :overweight_percentage, :obese_percentage
+
+  def initialize(data)
+    @uid                   = data[:uid]
+    @id                    = data[:id]
+    @overweight_percentage = data[:overweight_percentage]
+    @obese_percentage      = data[:obese_percentage]
   end
 
 end
