60ae994fcae06133faf63a37c36dc0fcef3dcea7
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-04 11:42:08 -0600

    added test for show method

--- spec/services/schoolist_service_spec.rb
+++ spec/services/schoolist_service_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe SchoolistService, type: :service do
     end
   end
 
+  it "returns a school" do
+    VCR.use_cassette("schoolist_service_school") do
+      school = SchoolistService.new.school(2)
+      expect(school[:id]).to eq(2)
+      expect(school[:overweight_percentage]).to eq("17.2")
+      expect(school[:obese_percentage]).to eq("25.4")
+      expect(school[:county_id]).to eq(1)
+    end
+
+  end
+
 end
