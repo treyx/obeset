e1d80bf5d5391fa6fd397484236031134f030953
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-04 11:20:13 -0600

    configured VCR in spec

--- spec/services/schoolist_service_spec.rb
+++ spec/services/schoolist_service_spec.rb
@@ -3,12 +3,14 @@ require "rails_helper"
 RSpec.describe SchoolistService, type: :service do
 
   it "returns the schools" do
-    schools = SchoolistService.new.schools
-    first_school = schools.first
-    expect(first_school[:id]).to eq(1)
-    expect(first_school[:overweight_percentage]).to eq("18.5")
-    expect(first_school[:obese_percentage]).to eq("20.8")
-    expect(first_school[:county_id]).to eq(1)
+    VCR.use_cassette("schoolist_service_schools") do
+      schools = SchoolistService.new.schools
+      first_school = schools.first
+      expect(first_school[:id]).to eq(1)
+      expect(first_school[:overweight_percentage]).to eq("18.5")
+      expect(first_school[:obese_percentage]).to eq("20.8")
+      expect(first_school[:county_id]).to eq(1)
+    end
   end
 
 end
