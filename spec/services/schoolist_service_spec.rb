c2dbeb8980b3a383c5d35694a652d14b22e4f68b
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-04 11:11:39 -0600

    basic implementation of index spec

--- spec/services/schoolist_service_spec.rb
+++ spec/services/schoolist_service_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe SchoolistService, type: :service do
 
   it "returns the schools" do
     schools = SchoolistService.new.schools
+    first_school = schools.first
+    expect(first_school[:id]).to eq(1)
+    expect(first_school[:overweight_percentage]).to eq("18.5")
+    expect(first_school[:obese_percentage]).to eq("20.8")
+    expect(first_school[:county_id]).to eq(1)
   end
 
 end
